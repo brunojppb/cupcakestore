27a5295f41e562bc011bc13781a9ff2cb4f803ca
Bruno Paulino <brunojppb@gmail.com>
2015-02-17 18:45:34 -0500

    User signin, signout and signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :set_locale
+
   include SessionsHelper
+
+
+  private
+    def set_locale
+      if logged_in?
+        I18n.default_locale = current_user.language
+      else
+        I18n.default_locale = :pt
+      end
+    end
 end
